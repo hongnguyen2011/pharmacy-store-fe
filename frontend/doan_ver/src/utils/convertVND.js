0fa1084bb4bc725a67462eb0e20d4ae1ed2cc392
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2024-08-20 10:56:19 +0700

    update

--- frontend/doan_ver/src/utils/convertVND.js
+++
@@ -1,4 +0,0 @@
-export const VND = new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-  });
---
+++ frontend/src/adminSide/pages/Home/LineChart.js
@@ -0,0 +1,47 @@
+import ReactApexChart from "react-apexcharts";
+import { Typography } from "antd";
+import lineChart from "./configs/lineChart";
+import { useEffect, useState } from "react";
+import axios from "axios";
+const LineChart = () => {
+  const { Title } = Typography;
+  const token = JSON.parse(localStorage.getItem("token"));
+  const [data, setData] = useState();
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: token,
+  };
+  useEffect(() => {
+    axios
+      .get(`https://localhost:7102/api/statistical/revenue`, { headers })
+      .then((respone) => {
+        if (respone.data.status === 200) {
+          setData(respone.data.data);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  const result = lineChart(data);
+  return (
+    <>
+      <div className="linechart">
+        <div>
+          <Title style={{ textAlign: "center" }} level={3}>
+            Thống kê doanh thu hàng tháng
+          </Title>
+        </div>
+      </div>
+
+      <ReactApexChart
+        className="full-width"
+        options={result.options}
+        series={result.series}
+        type="area"
+        height={350}
+        width={"100%"}
+      />
+    </>
+  );
+};
+
+export default LineChart;
